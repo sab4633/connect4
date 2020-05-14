aef7c3331febc1ba9dc92cf3fa7df7830535ba64
Sofia Bzhilyanskaya <sab4633@rit.edu>
2020-05-14 00:46:10 -0400

    printing matrices

--- ptui.py
+++ ptui.py
@@ -1,8 +1,16 @@
 #setup
-board = [7][6]
+import numpy as np
 
-def main:
+w, h = 7, 6;
+board = [[0 for x in range(w)] for y in range(h)]
+
+def main():
     print(board)
+    print_board(board)
+
+def print_board(board):
+    print(np.matrix(board))
+
 
 
 if __name__ == "__main__":
